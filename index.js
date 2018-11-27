9d62d49a172958aed05f4c2741fe912845c8b78b
Aurora <asdasdasdasd>
2018-11-27 16:44:13 +0100

    Add directory entities

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express'),
-    bodyParser = require('body-parser');
-
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000
+
 app.use(bodyParser.json());
 
-const register = require(./src/register.js);
-const login = require(./src/login.js);
+const register = require("./src/register.js");
+const login = require("./src/login.js");
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 //REGISTER
-const newUserRegistered = [];
-
-register.post ('/register', function(req, res){
+app.post ('/register', function(req, res){
+  res.send(register.addUser(req, res));
 })
 
-//LOGIN
+/*LOGIN
+login.post ('/login', function(req, res){
+
+})*/
+
+app.listen(PORT, () => console.log('App listening on port'+ PORT))
