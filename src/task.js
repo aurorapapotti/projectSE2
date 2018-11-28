80f7a86ce8ff8702a18cbdedb3de097d9dad7aa0
martidecet <decet.martina@gmail.com>
2018-11-28 01:05:51 +0100

    Task e TaskGroup

--- src/task.js
+++ src/task.js
@@ -8,28 +8,24 @@ const Task = (req, res) => {
         author: req.body.author
     };
 
-    if (!task.type || typeof task.type != 'string' || (task.type != 
-'Normal' && task.type != "Multiple_choice")) {
-        res.status(400).json({ error: 'The field "type" must be a 
-non-empty string. Possible types: "Normal", "Multiple_choice' });
+    if (!task.type || typeof task.type != 'string' || (task.type != 'Normal' && task.type != "Multiple_choice")) {
+        res.status(400).json({ error: 'The field "type" must be a non-empty string. Possible types: "Normal", 
+"Multiple_choice' });
         return;
     }
 
     if (!task.argument || typeof task.argument != 'string') {
-        res.status(400).json({ error: 'The field "argument" must be a 
-string' });
+        res.status(400).json({ error: 'The field "argument" must be a string' });
         return;
     }
 
     if (!task.description || typeof task.description != 'string') {
-        res.status(400).json({ error: 'The field "description" must be a 
-non-empty string' });
+        res.status(400).json({ error: 'The field "description" must be a non-empty string' });
         return;
     }
 	
 	if (!task.author || typeof task.author != 'string') {
-        res.status(400).json({ error: 'The field "author" must be a  
-non-empty string' });
+        res.status(400).json({ error: 'The field "author" must be a  non-empty string' });
         return;
     }
 };
--- src/taskGroup.js
+++ src/taskGroup.js
@@ -9,20 +9,17 @@ const TaskGroup = (req, res) => {
 
 
     if (!task.name || typeof task.name != 'string') {
-        res.status(400).json({ error: 'The field "name" must be a 
-string' });
+        res.status(400).json({ error: 'The field "name" must be a string' });
         return;
     }
 
     if (!task.tasks || typeof task.tasks != 'string') {
-        res.status(400).json({ error: 'The field "tasks" must be a 
-non-empty string' });
+        res.status(400).json({ error: 'The field "tasks" must be a non-empty string' });
         return;
     }
 	
 	if (!task.author || typeof task.author != 'string') {
-        res.status(400).json({ error: 'The field "author" must be a  
-non-empty string' });
+        res.status(400).json({ error: 'The field "author" must be a  non-empty string' });
         return;
     }
 };
