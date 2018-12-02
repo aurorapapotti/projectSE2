990dd7ca3565ccd6ec472872c8efacd43ce8361d
Elena Tomaiuolo <elena.tomaiuolo@live.it>
2018-12-02 17:40:25 +0100

    createReview and getReview with JSON

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const register = require("./src/register.js");
 const login = require("./src/login.js");
+const review = require("./src/review.js");
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -19,5 +21,27 @@ app.post ('/register', function(req, res){
 app.post ('/login', function(req, res){
   res.send(login.loginUser(req,res));
 })
+
+//REVIEW
+app.post("/reviews", (req, res) => {
+  res.send(review.createReview(req, res));
+})
+
+app.get("/reviews", (req, res) => {
+  res.send(review.getAllReviews(req, res));  
+})
+
+app.get("/reviews/:reviewId", (req, res) => {
+  res.send(review.getReview(req, res));
+});
+
+app.get("/reviews/:reviewId/peerReviews", (req, res) => {
+
+});
+
+app.get("/reviews/:reviewId/peerReviews/:peerReviewId", (req, res) => {
+
+});
+
 
 app.listen(PORT, () => console.log('App listening on port'+ PORT))
--- src/review.js
+++ src/review.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const express = require("express");
 const bodyParser = require("body-parser");
-const percistencyLayer = require("./persistencyLayer.js");
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,7 +9,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = process.env.PORT || 3000;
 
-app.post("/review", (req, res) => {
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+})
+
+function createReview (req, res) {
 	var taskAnswer = req.body.taskAnswer;
 	var peerReview = req.body.peerReview;
 	
@@ -23,15 +29,50 @@ app.post("/review", (req, res) => {
 		
 	percistencyLayer.writeReview(review);
 	res.status(201).send("Created");
-});
-
-app.get("/reviews", (req, res) => {
-	const reviews = percistencyLayer.getAllReview(req.query);
-	res.json(reviews);
-});
-
-module.exports = {
-	app
 }
 
-app.listen(PORT, () => console.log("App listening on PORT "+ PORT));
+function getAllReviews (req, res)  {
+	return percistencyLayer.getAllReview(req.query);
+}
+
+function getReview (req, res) {
+	return percistencyLayer.getReview(req.params.reviewId);
+};
+/*
+app.put("/reviews/:reviewId", (req, res) => {
+
+});
+
+
+app.delete("/reviews/:reviewId", (req, res) => {
+
+});
+*/
+app.get("/reviews/:reviewId/peerReviews", (req, res) => {
+	const review = percistencyLayer.getReview(req.params.reviewId);
+
+});
+
+app.get("/reviews/:reviewId/peerReviews/:peerReviewId", (req, res) => {
+
+});
+
+/*
+app.put("/reviews/:reviewId/peerReviews/:peerReviewId", (req, res) => {
+
+});
+
+app.get("/reviews/:reviewId/taskAnswers/:taskAnswerId", (req, res) => {
+
+});
+
+app.put("/reviews/:reviewId/taskAnswers/:taskAnswerId", (req, res) => {
+
+});
+*/
+module.exports = {
+	createReview: createReview,
+	getAllReviews: getAllReviews
+}
+
+//app.listen(PORT, () => console.log("App listening on PORT "+ PORT));
