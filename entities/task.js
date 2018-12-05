b6f11f6bae1b85697d9ebf5ae533acdbbed485cf
martidecet <decet.martina@gmail.com>
2018-12-05 17:29:34 +0100

    Sistemato entities/Task.js

--- entities/task.js
+++ entities/task.js
@@ -2,14 +2,14 @@
 	"Task1":{
 		"id":"1",
 		"argument": "Ingegneria del software",
-		"text": "abcdef",
+		"Description": "abcdef",
 		"taskType": "Task1",
 		"author": "User"
 	}
 	"Task2":{
                 "id":"2",
                 "argument": "Ingegneria del software2",
-                "text": "abcdefx2",
+                "description": "abcdefx2",
                 "taskType": "Task2",
                 "author": "User2"
         }
