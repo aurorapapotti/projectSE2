62d45ddbdec80ac96be3b903473ab47bb4b51628
Elena Tomaiuolo <elena.tomaiuolo@live.it>
2018-12-05 14:45:08 +0100

    Modificato index.js

--- index.js
+++ index.js
@@ -14,106 +14,56 @@ const taskAnswer = require("./src/taskAnswer.js");
 app.get('/', (req, res) => res.send('Hello World!'))
 
 //REGISTER
-app.post ('/register', function(req, res){
-  res.send(register.registerUser(req, res));
-})
+app.post ('/register', register.registerUser(req, res));
 
 //LOGIN
-app.post ('/login', function(req, res){
-  res.send(login.loginUser(req,res));
-})
+app.post ('/login', login.loginUser(req,res));
 
 //REVIEW
-app.post("/reviews", (req, res) => {
-  res.send(review.createReview(req, res));
-})
+app.post("/reviews", review.createReview(req, res));
 
-app.get("/reviews", (req, res) => {
-  res.send(review.getAllReviews(req, res));  
-})
+app.get("/reviews", review.getAllReviews(req, res));  
 
-app.get("/reviews/:reviewId", (req, res) => {
-  res.send(review.getReview(req, res));
-});
+app.get("/reviews/:reviewId", review.getReview(req, res));
 
-app.get("/reviews/:reviewId/peerReviews", (req, res) => {
-  res.send(review.getAllPeerReviews(req, res));
-});
+app.get("/reviews/:reviewId/peerReviews", review.getAllPeerReviews(req, res));
 
-app.get("/reviews/:reviewId/peerReviews/:peerReviewId", (req, res) => {
-  res.send(review.getPeerReview(req, res));
-});
+app.get("/reviews/:reviewId/peerReviews/:peerReviewId", review.getPeerReview(req, res));
 
-app.get("/reviews/:reviewId/taskAnswers", (req, res) => {
-  res.send(review.getAllTaskAnswers(req, res));
-});
+app.get("/reviews/:reviewId/taskAnswers", review.getAllTaskAnswers(req, res));
 
-app.get("/reviews/:reviewId/taskAnswers/:taskAnswerId", (req, res) => {
-  res.send(review.getTaskAnswer(req, res));
-});
+app.get("/reviews/:reviewId/taskAnswers/:taskAnswerId", review.getTaskAnswer(req, res));
 
-app.delete("/reviews/:reviewId", (req, res) => {
-  res.send(review.deleteReview(req, res));
-});
+app.delete("/reviews/:reviewId", review.deleteReview(req, res));
 
-app.put("/reviews/:reviewId/peerReviews/:peerReviewId", (req, res) => {
-  res.send(review.addPeerReview(req, res));
-});
+app.put("/reviews/:reviewId/peerReviews/:peerReviewId", res.send(review.addPeerReview(req, res));
 
-app.put("/reviews/:reviewId/taskAnswers/:taskAnswerId", (req, res) => {
-  res.send(review.addTaskAnswer(req, res));
-});
+app.put("/reviews/:reviewId/taskAnswers/:taskAnswerId", review.addTaskAnswer(req, res));
 
-app.post("/taskAnswers", (req, res) => {
-  res.send(taskAnswer.createTaskAnswer(req, res));
-});
+app.post("/taskAnswers", taskAnswer.createTaskAnswer(req, res));
 
-app.get("/taskAnswers", (req, res) => {
-  res.send(taskAnswer.getAllTaskAnswers(req, res));
-});
+app.get("/taskAnswers", taskAnswer.getAllTaskAnswers(req, res));
 
-app.get("/taskAnswers/:taskAnswerId", (req, res) => {
-  res.send(taskAnswer.getTaskAnswer(req, res));
-});
+app.get("/taskAnswers/:taskAnswerId", taskAnswer.getTaskAnswer(req, res));
 
-app.get("/taskAnswers/:taskAnswerId/answers", (req, res) => {
-  res.send(taskAnswer.getAllAnswers(req, res));
-});
+app.get("/taskAnswers/:taskAnswerId/answers", taskAnswer.getAllAnswers(req, res));
 
-app.get("/taskAnswers/:taskAnswerId/answers/:answerId", (req, res) => {
-  res.send(taskAnswer.getAnswer(req, res));
-});
+app.get("/taskAnswers/:taskAnswerId/answers/:answerId", taskAnswer.getAnswer(req, res));
 
-app.get("/taskAnswers/:taskAnswerId/student", (req, res) => {
-  res.send(taskAnswer.getStudent(req, res));
-});
+app.get("/taskAnswers/:taskAnswerId/student", taskAnswer.getStudent(req, res));
 
-app.get("/taskAnswers/:taskAnswerId/assignment", (req, res) => {
-  res.send(taskAnswer.getAssignment(req, res));
-});
+app.get("/taskAnswers/:taskAnswerId/assignment", taskAnswer.getAssignment(req, res));
 
-app.get("/taskAnswers/:taskAnswerId/taskGroup", (req, res) => {
-  res.send(taskAnswer.getTaskGroup(req, res));
-});
+app.get("/taskAnswers/:taskAnswerId/taskGroup", taskAnswer.getTaskGroup(req, res));
 
-app.delete("/taskAnswers/:taskAnswerId", (req, res) => {
-  res.send(taskAnswer.deleteTaskAnswer(req, res));
-});
+app.delete("/taskAnswers/:taskAnswerId", taskAnswer.deleteTaskAnswer(req, res));
 
-app.put("/taskAnswers/:taskAnswerId/answers", (req, res) => {
-  res.send(taskAnswer.addAnswer(req, res));
-});
+app.put("/taskAnswers/:taskAnswerId/answers", taskAnswer.addAnswer(req, res));
 
-app.put("/taskAnswers/:taskAnswerId/assignment", (req, res) => {
-  res.send(taskAnswer.editAssignment(req, res));
-});
+app.put("/taskAnswers/:taskAnswerId/assignment", taskAnswer.editAssignment(req, res));
 
-app.put("/taskAnswers/:taskAnswerId/taskGroup", (req, res) => {
-  res.send(taskAnswer.editTaskGroup(req, res));
-});
+app.put("/taskAnswers/:taskAnswerId/taskGroup", taskAnswer.editTaskGroup(req, res));
 
-app.put("/taskAnswers/:taskAnswerId/student", (req, res) => {
-  res.send(taskAnswer.editStudent(req, res));
-})
+app.put("/taskAnswers/:taskAnswerId/student", taskAnswer.editStudent(req, res));
 
 app.listen(PORT, () => console.log('App listening on port '+ PORT))
