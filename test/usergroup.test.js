0c9d93ddc9557588215a062d6db23d8bf4cc11b6
silviamorselli <silvia.morselli@studenti.unitn.it>
2018-12-08 20:39:00 +0100

    aggiunti test per GET, DELETE, POST di userGroup

--- test/usergroup.test.js
+++ test/usergroup.test.js
@@ -1,146 +1,290 @@
-//--- GET USER GROUP ---
-const getUsers = require('./userGroup').getUsers
-.........................................
 
+const usergroupFunc = require('../src/functionsEntities/userGroupFunctions.js');
+const userFunc = require('../src/functionsEntities/userFunctions.js');
+const usergroup = require("../src/usergroup.js");
+const user = require("../src/user.js");
 
-//--- POST USER GROUP ---
-const createUsers = require('./userGroup').createUsers
+const res = {
+	"status": (statuscode) =>{ return {
+		"json": (list) => {return {
+			"code": statuscode,
+			"list": list
+		}}
+	}}
+}
 
-test('create userGroup (author pos, users pos)', () => {
-	var author = 1;
-	var users = 1;
-	
-	expect(createUsers(author, users)).toBe(200);
+const userGroup = {
+	name: "classe 1",
+	author: "user1",
+	users: ["user1", 
+			"user2", 
+			"user3"]
+}
+
+//	/userGroup
+describe ('GET /userGroup valid tests', () =>{
+	test('GET /userGroup return code 200', () => {
+		var req = {};
+		expect(usergroup.listUserGroups({"body": req}, res)).toEqual(res.status(200).json(usergroupFunc.getAllUserGroups));
+	})
 });
 
-test('create userGroup (author null, users pos)', () => {
-	var author = null;
-	var users = 1;
-	
-	expect(createUsers(author, users)).toBe(404);
+describe ('GET /userGroup invalid tests', () =>{
+	test('GET /userGroup req undefined', () => {
+		expect(usergroup.listUserGroups(undefined, res)).toEqual(res.status(400).json("Bad Request"));
+	})
 });
 
-test('create userGroup (author pos, users null)', () => {
-	var author = 1;
-	var users = null;
-	
-	expect(createUsers(author, users)).toBe(404);
+describe ('POST /userGroup valid tests', () =>{
+	test('POST /userGroup valid creation', () => {
+		expect(userGroup.createUserGroup({"params":{"name": nome, "author": author, "users": users}}, res)).toEqual(res.status(200).json("Created."));
+	})
 });
 
-test('create userGroup (author null, users null)', () => {
-	var author = null;
-	var users = null;
-	
-	expect(createUsers(author, users)).toBe(404);
+describe ('POST /userGroup invalid tests', () =>{
+	test('POST /userGroup name not string', () => {
+		var name = 1;
+		expect(userGroup.createUserGroup({"params":{"name": nome, "author": author, "users": users}}, res)).toEqual(res.status(400).json("Bad request"));
+	})
+
+	test('POST /userGroup author null', () => {
+		var us = usergroup.getUserGroupById(userGroupId);
+		var author = us["author"];
+		if(author == null)
+			expect(userGroup.createUserGroup({"params":{"name": nome, "author": author, "users": users}}, res)).toEqual(res.status(400).json("Bad request"));
+	})
+
+	test('POST /userGroup users null', () => {
+		var us = usergroup.getUserGroupById(userGroupId);
+		var users = us["users"];
+		if(users == null)
+			expect(userGroup.createUserGroup({"params":{"name": nome, "author": author, "users": users}}, res)).toEqual(res.status(400).json("Bad request"));
+	})
 });
 
-
-//--- PUT USER GROUP ---
-const editUsers = require('./userGroup').editUsers
-
-test('edit users (old null, new pos)', () => {
-	var oldAss = null;
-	var newAss = 3;
-	
-	expect(editUsers(oldAss, newAss)).toBe(404);
+//	/userGroup/:userGroupId
+describe ('GET /userGroup/:userGroupId valid tests', () =>{
+	test('GET /userGroup/:userGroupId return code 200', () => {
+		var userGroupId = usergroup.createUserGroup(req, res);
+		var us = usergroup.getUserGroupById(userGroupId);
+		expect(usergroup.getUserGroupById({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(200).json(us));
+	})
 });
 
-test('edit users (old pos, new null)', () => {
-	var oldAss = 3;
-	var newAss = null;
-	
-	expect(editUsers(oldAss, newAss)).toBe(404);
+describe ('GET /userGroup/:userGroupId invalid tests', () =>{
+	test('GET /userGroup/:userGroupId userGroup NOT found', () => {
+		var userGroupId = "_ciao";
+		expect(usergroup.getUserGroupById({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(404).json("UserGroup NOT Found"));
+	})
+
+	test('GET /userGroup/:userGroupId usergroupId is a number', () => {
+		var userGroupId = 834389;
+		expect(usergroup.getUserGroupById({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('GET /userGroup/:userGroupId usergroupId is undefined', () => {
+		var userGroupId = undefined;
+		expect(usergroup.getUserGroupById({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('GET /userGroup/:userGroupId usergroupId is NULL', () => {
+		var userGroupId = null;
+		expect(usergroup.getUserGroupById({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('GET /userGroup/:userGroupId params is undefined', () => {
+		var userGroupId = "_jhdsd4783x";
+		expect(usergroup.getUserGroupById({"": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('GET /userGroup/:userGroupId req is undefined', () => {
+		expect(usergroup.getUserGroupById(undefined, res)).toEqual(res.status(400).json("Bad Request"));
+	})
 });
 
-test('edit users (old null, new null)', () => {
-	var oldAss = null;
-	var newAss = null;
-	
-	expect(editUsers(oldAss, newAss)).toBe(404);
+describe ('PUT /userGroup/:userGroupId valid tests', () =>{
+
 });
 
-test('edit users (old pos, new pos)', () => {
-	var oldAss = 4;
-	var newAss = 3;
-	
-	expect(editUsers(oldAss, newAss)).toBe(200);
+describe ('PUT /userGroup/:userGroupId invalid tests', () =>{
+
 });
 
-
-//--- GET USER_GROUP/ID ---
-const getId = require('./userGroup/userGroupId').getId
-............................
-
-
-//--- PUT USER_GROUP/ID ---
-const putUsers = require('./userGroup/userGroupId').putUsers
-
-test('edit users (old null, new pos)', () => {
-	var oldAss = null;
-	var newAss = 3;
-	
-	expect(putUsers(oldAss, newAss)).toBe(404);
+describe ('DELETE /userGroup/:userGroupId valid tests', () =>{
+	test('DELETE /userGroup/:userGroupId return code 200', () =>{
+		var userGroupId = usergroupFunc.createUserGroup(userGroup);
+		expect(usergroup.deleteUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(200).json("UserGroup Deleted."));
+	})
 });
 
-test('edit users (old pos, new null)', () => {
-	var oldAss = 3;
-	var newAss = null;
-	
-	expect(putUsers(oldAss, newAss)).toBe(404);
+describe ('DELETE /userGroup/:userGroupId invalid tests', () =>{
+	test('DELETE /userGroup/:userGroupId userGroup NOT found', () => {
+		var userGroupId = "_ciao";
+		expect(usergroup.deleteUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(404).json("UserGroup NOT Found"));
+	})
+
+	test('DELETE /userGroup/:userGroupId usergroupId is a number', () => {
+		var userGroupId = 834389;
+		expect(usergroup.deleteUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId usergroupId is undefined', () => {
+		var userGroupId = undefined;
+		expect(usergroup.deleteUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId usergroupId is NULL', () => {
+		var userGroupId = null;
+		expect(usergroup.deleteUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId params is undefined', () => {
+		var userGroupId = "_jhdsd4783x";
+		expect(usergroup.deleteUserGroup({"": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId req is undefined', () => {
+		expect(usergroup.deleteUserGroup(undefined, res)).toEqual(res.status(400).json("Bad Request"));
+	})
 });
 
-test('edit users (old null, new null)', () => {
-	var oldAss = null;
-	var newAss = null;
-	
-	expect(putUsers(oldAss, newAss)).toBe(404);
+//	/userGroup/:userGroupId/author
+describe ('GET /userGroup/:userGroupId/author valid tests ', () =>{
+	test('GET /userGroup/:userGroupId/author valid author', () => {
+		var id = "_jhdsd4783x";
+		var author = usergroup.getAuthorByIdUserGroup(id);
+		expect(usergroup.getAuthorByIdUserGroup({"param": {"idUserGroup": userGroupId, "author": author}}, res)).toEqual(res.status(200).json("Author received."));
+	})
 });
 
-test('edit users (old pos, new pos)', () => {
-	var oldAss = 4;
-	var newAss = 3;
-	
-	expect(putUsers(oldAss, newAss)).toBe(200);
+describe ('GET /userGroup/:userGroupId/author invalid tests', () =>{
+	test('DELETE /userGroup/:userGroupId/author userGroup NOT found', () => {
+		var userGroupId = "_ciao";
+		expect(usergroup.getAuthorByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(404).json("UserGroup NOT Found"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/author usergroupId is a number', () => {
+		var userGroupId = 834389;
+		expect(usergroup.getAuthorByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/author usergroupId is undefined', () => {
+		var userGroupId = undefined;
+		expect(usergroup.getAuthorByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/author usergroupId is NULL', () => {
+		var userGroupId = null;
+		expect(usergroup.getAuthorByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/author params is undefined', () => {
+		var userGroupId = "_jhdsd4783x";
+		expect(usergroup.getAuthorByIdUserGroup({"": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
 });
 
-
-//--- DELETE USER_GROUP/ID ---
-const deleteId = require('./userGroup/userGroupId').deleteId
-
-test('delete UserGroup (id null)', () => {
-	var id = null;
-	
-	expect(deleteId(id)).toBe(404);
+//	/userGroup/:userGroupId/users
+describe ('GET /userGroup/:userGroupId/users valid', () =>{
+	test('GET /userGroup/:userGroupId/users valid users', () => {
+		var userGroupId = "_jhdsd4783x";
+		expect(usergroup.getUsersByIdUserGroup({"param": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(200).json("Users received."));
+	})
 });
 
-test('delete UserGroup (id pos)', () => {
-	var id = 1;
-	
-	expect(deleteId(id)).toBe(200);
+describe ('GET /userGroup/:userGroupId/users invalid', () =>{
+	test('DELETE /userGroup/:userGroupId/users userGroup NOT found', () => {
+		var userGroupId = "_ciao";
+		expect(usergroup.getUsersByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(404).json("UserGroup NOT Found"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users usergroupId is a number', () => {
+		var userGroupId = 834389;
+		expect(usergroup.getUsersByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users usergroupId is undefined', () => {
+		var userGroupId = undefined;
+		expect(usergroup.getUsersByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users usergroupId is NULL', () => {
+		var userGroupId = null;
+		expect(usergroup.getUsersByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users params is undefined', () => {
+		var userGroupId = "_jhdsd4783x";
+		expect(usergroup.getUsersByIdUserGroup({"": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users params is undefined', () => {
+		var userGroupId = "_jhdsd4783x";
+		expect(usergroup.getUsersByIdUserGroup({"": {"idUserGroup": userGroupId, "users": []}}, res)).toEqual(res.status(404).json("Users NOT found"));
+	})
 });
 
-
-//--- GET USER_GROUP/AUTHOR ---
-const getAuthor = require('./userGroup/Author').getAuthor
-...................................
-
-
-//--- GET USERS ---
-const getUsers = require('./userGroup/userGroupId/users').getUsers
-....................................
-
-
-//--- DELETE A USER ---
-const deleteUser = require('./userGroup/userGroupId/users/userId').deleteUser
-
-test('delete User (id null)', () => {
-	var id = null;
-	
-	expect(deleteUser(id)).toBe(404);
+//	/userGroup/:userGroupId/users/:userId
+describe ('DELETE /userGroup/:userGroupId/users/:userId valid', () =>{
+	test('DELETE /userGroup/:userGroupId/users/:userId return code 200', () =>{
+		var userGroupId = usergroupFunc.getUserGroupById(userGroup);
+		var userId = userFunc.getUserById(user);
+		expect(usergroup.deleteUserGroup({"params": {"idUserGroup": userGroupId, "idUser": userId}}, res)).toEqual(res.status(200).json("User Deleted from userGroup."));
+	})
 });
 
-test('delete User (id pos)', () => {
-	var id = 1;
-	
-	expect(deleteUser(id)).toBe(200);
+describe ('DELETE /userGroup/:userGroupId/users/:userId invalid', () =>{
+	test('DELETE /userGroup/:userGroupId/users/:userId userGroup NOT found', () => {
+		var userGroupId = "_ciao";
+		expect(usergroup.deleteUserByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(404).json("UserGroup NOT Found"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId userGroupId is a number', () => {
+		var userGroupId = 834389;
+		expect(usergroup.deleteUserByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId usergroupId is undefined', () => {
+		var userGroupId = undefined;
+		expect(usergroup.deleteUserByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId usergroupId is NULL', () => {
+		var userGroupId = null;
+		expect(usergroup.deleteUserByIdUserGroup({"params": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId params is undefined', () => {
+		var userGroupId = "_jhdsd4783x";
+		expect(usergroup.deleteUserByIdUserGroup({"": {"idUserGroup": userGroupId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId req is undefined', () => {
+		expect(usergroup.deleteUserByIdUserGroup(undefined, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId user NOT found', () => {
+		var userGroupId = "_jhdsd4783x";
+		var userId = "_ciao";
+		expect(usergroup.deleteUserByIdUserGroup({"params": {"idUserGroup": userGroupId, "idUser": userId}}, res)).toEqual(res.status(404).json("User NOT Found"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId userGroupId is a number', () => {
+		var userGroupId = "_jhdsd4783x";
+		var userId = 834389;
+		expect(usergroup.deleteUserByIdUserGroup({"params": {"idUserGroup": userGroupId, "idUser": userId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId usergroupId is undefined', () => {
+		var userGroupId = "_jhdsd4783x";
+		var userId = undefined;
+		expect(usergroup.deleteUserByIdUserGroup({"params": {"idUserGroup": userGroupId, "idUser": userId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
+
+	test('DELETE /userGroup/:userGroupId/users/:userId usergroupId is NULL', () => {
+		var userGroupId = "_jhdsd4783x";
+		var userId = null;
+		expect(usergroup.deleteUserByIdUserGroup({"params": {"idUserGroup": userGroupId, "idUser": userId}}, res)).toEqual(res.status(400).json("Bad Request"));
+	})
 });
+
